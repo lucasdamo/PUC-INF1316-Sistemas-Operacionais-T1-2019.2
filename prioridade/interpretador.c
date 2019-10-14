03ed0a8efea35a1d21d7d902e9581d44e6450c6d
Lucas Damo <lucasdamo1@gmail.com>
2019-10-14 00:10:06 -0300

    Interpredator agora é capaz de ler os comandos no arquivo .txt, prepara-los e mandar via pipe ao escalonador

--- prioridade/interpretador.c
+++ prioridade/interpretador.c
@@ -15,18 +15,28 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include "../fila/fila.h"
 #define DEBUG 1
 #define NUM_PALAVRAS_ESPERADO 3 // Numero de palavras esperado em cada linha do arquivo txt inputado
+#define TRUE 1
  
+typedef struct comando {
+	char com[255];
+	int prioridade;
+} Comando;
 
-
+int p[2];
 
 
 void interpretaComandos(char * linha){
-	char * palavra;
-	char * saveptr; // Variavel para uso interno do strtok
+	char * palavra, * palavraprd;
+	char * saveptr, * saveptr2; // Variavel para uso interno do strtok
 	char * argumentos[NUM_PALAVRAS_ESPERADO];
-	int contaPalavra = 0, i;
+	Comando * cmm;
+	int contaPalavra = 0;
+
+	cmm = (Comando*)malloc(sizeof(Comando));
 	palavra = strtok_r(linha, " \n", &saveptr);
 	while(palavra != NULL){
 		argumentos[contaPalavra] = palavra;
@@ -40,20 +50,38 @@ void interpretaComandos(char * linha){
 	if(strcmp(argumentos[0], "Run") != 0){
 		fprintf(stderr, "Comando %s não começa com Run, e sim com %s\n", linha, argumentos[0]);
 		exit(EXIT_FAILURE);	
-	}	
+	}
+	palavraprd = strtok_r(argumentos[2], "PR=", &saveptr2);
+	strcpy(cmm->com, argumentos[1]);
+	sscanf(palavraprd, "%d", &cmm->prioridade);
+	write(p[1], cmm, sizeof(Comando));
+	#ifdef DEBUG
+	printf("Escrito comando %s prioridade %d\n", cmm->com, cmm->prioridade);
+	#endif
+
 }
 
 
 
 
 void childHandler(){
-
-
-
 	#ifdef DEBUG
 		printf("Processo filho iniciado com pid %d\n", getpid());
-		
 	#endif
+	Comando * buff;
+	int nBytes, i;
+
+	buff = (Comando *)malloc(sizeof(Comando));
+
+	while( ( nBytes = read(p[0], buff, sizeof(Comando)) ) != 0 ){
+		#ifdef DEBUG
+			printf("Comando %s recebido com prioridade %d\n", buff->com, buff->prioridade);
+		#endif
+	}
+	sleep(5);
+	for(i=0; i<1000; i++){}
+	
+	
 
 }
 
@@ -63,11 +91,11 @@ void parentHandler(FILE *fp){
 	ssize_t charLidos; // Quantidade de caracteres lidos
 	#ifdef DEBUG
 		printf("Processo pai iniciado com pid %d\n", getpid());
-		
 	#endif
 
 	while ((charLidos = getline(&linha, &tam, fp)) != -1){
 		interpretaComandos(linha);
+
 	}
 	free(linha); // getLine aloca memoria automaticamente
 
@@ -85,6 +113,11 @@ int main(int argc, char *argv[]){
 	FILE *fp; // Ponteiro para o arquivo a ser aberto
 	pid_t flagFork;
 
+	if(pipe(p) < 0){
+		fprintf(stderr, "Erro ao realizar o pipe para a comunicacao\n");
+		exit(EXIT_FAILURE);	
+	}
+
 	fp = fopen("exec.txt", "r");
 	if (fp == NULL){
 		fprintf(stderr, "Erro ao abrir o arquivo exec.txt\n");
@@ -98,10 +131,12 @@ int main(int argc, char *argv[]){
 	}
 	else if (flagFork == 0){
 		/* Processo filho */
+		close(p[1]);
 		childHandler();
 	}
 	else {
 		/* Processo pai */
+		close(p[0]);
 		parentHandler(fp);
 	}
 
